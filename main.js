9ee17caf31ef292f8556fb34fa057db2c80c8b44
Will Chislett <will.chislett@teamlewis.com>
2020-08-12 13:38:54 -0700

    initial commit with test files.

---
+++ main.js
@@ -0,0 +1,17 @@
+/*****************************************************************************************
+* Part 2
+****************************************************************************************/
+var employees = [
+        {first: "Amanda", last: "Byron", group: "Sales"},
+        {first: "Ye", last: "Xia", group: "Receiving", nameOrder: "reverse"},
+        {first: "Miltiades", last: "Crescens", group: "Sales"},
+        /*...don't foget to account for other entries of the same form, but with different group names.....*/
+    ];
+
+// Part 2 Answer Here
+
+/*****************************************************************************************
+* Bonus
+****************************************************************************************/
+
+// Bonus Anwser Here
